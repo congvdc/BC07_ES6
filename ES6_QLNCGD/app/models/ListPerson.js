5f79f6da168592d9094ca29d9c981c44c231cb9a
Chí Công <chicongtkn@gmail.com>
2023-06-17 22:10:18 +0700

    test

---
+++ ES6_QLNCGD/app/models/ListPerson.js
@@ -0,0 +1,15 @@
+
+
+import Customer from "./Customer.js";
+import DSHS from "./DSHS.js";
+import DSKH from "./DSKH.js";
+import DSNV from "./DSNV.js";
+
+export default class ListPerson {
+    constructor(arrHS, arrNV, arrKH) {
+        this.arrHS = arrHS;
+        this.arrNV = arrNV;
+        this.arrKH = arrKH;
+      }
+}
+
