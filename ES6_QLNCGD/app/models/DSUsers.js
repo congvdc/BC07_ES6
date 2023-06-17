e8bfe59137e8e9bfa672704878e5d62de8e8b34a
Chí Công <chicongtkn@gmail.com>
2023-06-17 23:05:24 +0700

    unknow

---
+++ ES6_QLNCGD/app/models/DSUsers.js
@@ -0,0 +1,8 @@
+import Person from "./Person";
+
+export default class DSUsers {
+    constructor (id, ten, diaChi, email, loai) {
+        this.arrPerson = [];
+        console.log(arrPerson);
+    }
+}
